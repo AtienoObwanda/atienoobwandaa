23aeebd6a9bda71e6fdb49cba7d7fca3cb4d1f1e
AtienoObwanda <atienoobwanda@gmail.com>
2023-10-24 09:23:54 +0300

    Outlining the design for experience component

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -7,28 +7,92 @@ const Experience = () => {
     <h1 className='text-6xl  text-center text-dark dark:text-white font-sans leading-normal'>Experience</h1>
 
     </div>
-<div className="flex">
-    <h6 className='text-xl	text-dark dark:text-white font-sans leading-normal	mt-[1.5em] pl-8 pr-8 pt-4 pb-4'>
-      Python
-    </h6>
 
-    <h6 className='text-xl	text-dark dark:text-white font-sans leading-normal	mt-[1.5em] pl-8 pr-8 pt-4 pb-4'>
-      Python
-    </h6>
 
-    <h6 className='text-xl	text-dark dark:text-white font-sans leading-normal	mt-[1.5em] pl-8 pr-8 pt-4 pb-4'>
-      Python
-    </h6>
+    <main class="relative min-h-screen flex flex-col justify-center  overflow-hidden">
+        <div class="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
+            <div class="flex flex-col justify-center divide-y divide-slate-200 [&>*]:py-16">
 
-    <h6 className='text-xl	text-dark dark:text-white font-sans leading-normal	mt-[1.5em] pl-8 pr-8 pt-4 pb-4'>
-      Python
-    </h6>
+                <div class="w-full max-w-3xl mx-auto">
+                
+                    <div class="my-6">
 
-    
-   
 
-  
-</div>
+                        {/* Freelance */}
+                        <div class="relative pl-8 sm:pl-32 py-6 group">
+                            {/* <!-- Purple label --> */}
+                            <div class="font-caveat font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">Freelancing</div>
+                            {/* <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) --> */}
+                            <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                                <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">May, 2020</time>
+                                <div class="text-xl font-bold text-slate-900">Software Developer | DevOps Engineer</div>
+                            </div>
+                            {/* <!-- Content --> */}
+                            <div class="text-slate-500">Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.</div>
+                        </div>
+
+                        {/* Fintech */}
+                        <div class="relative pl-8 sm:pl-32 py-6 group">
+                            {/* <!-- Purple label --> */}
+                            <div class="font-caveat font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">Fintech Group</div>
+                            {/* <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) --> */}
+                            <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                                <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">May, 2020</time>
+                                <div class="text-xl font-bold text-slate-900">Implementation & Support Engineer</div>
+                            </div>
+                            {/* <!-- Content --> */}
+                            <div class="text-slate-500">Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.</div>
+                        </div>
+
+                       
+                        
+                        {/* BetechPad */}
+                        <div class="relative pl-8 sm:pl-32 py-6 group">
+                            {/* <!-- Purple label --> */}
+                            <div class="font-caveat font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">Betechpad</div>
+                            {/* <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) --> */}
+                            <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                                <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">May, 2021</time>
+                                <div class="text-xl font-bold text-slate-900">Software Developer</div>
+                            </div>
+                            {/* <!-- Content --> */}
+                            <div class="text-slate-500">Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.</div>
+                        </div>
+                        
+                        {/* Opinion Space Africa */}
+                        <div class="relative pl-8 sm:pl-32 py-6 group">
+                            {/* <!-- Purple label --> */}
+                            <div class="font-caveat font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">Opinion Space Africa</div>
+                            {/* <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) --> */}
+                            <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                                <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">May, 2022</time>
+                                <div class="text-xl font-bold text-slate-900">Technical Support Specialist</div>
+                            </div>
+                            {/* <!-- Content --> */}
+                            <div class="text-slate-500">Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.</div>
+                        </div>
+                        
+                        {/* Afrovas Research House */}
+                        <div class="relative pl-8 sm:pl-32 py-6 group">
+                            {/* <!-- Purple label --> */}
+                            <div class="font-caveat font-medium text-2xl text-indigo-500 mb-1 sm:mb-0">Afrivas Reaseach House</div>
+                            {/* <!-- Vertical line (::before) ~ Date ~ Title ~ Circle marker (::after) --> */}
+                            <div class="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                                <time class="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">May, 2023</time>
+                                <div class="text-xl font-bold text-slate-900">Acme went public at the New York Stock Exchange</div>
+                            </div>
+                            {/* <!-- Content --> */}
+                            <div class="text-slate-500">Pretium lectus quam id leo. Urna et pharetra pharetra massa massa. Adipiscing enim eu neque aliquam vestibulum morbi blandit cursus risus.</div>
+                        </div>
+
+                    </div>
+                    
+                </div>
+
+
+            </div>
+        </div>
+    </main>
  
 
 </div>
