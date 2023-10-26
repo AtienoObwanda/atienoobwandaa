bf4133cad56b23c2a12265b2b92994cee3c82c39
AtienoObwanda <atienoobwanda@gmail.com>
2023-10-26 09:51:20 +0300

    removing footer dark color

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
             <h3 className="text-right text-dark dark:text-white mr-[3px]">Made by Millicent A.Obwanda | </h3>
             </div>
             <div className="w-1/2">
-            <h3 text-dark dark:text-white> Copyright © {year}</h3>
+            <h3 className="text-dark dark:text-white"> Copyright © {year}</h3>
             </div>
         </div>
     </footer>
