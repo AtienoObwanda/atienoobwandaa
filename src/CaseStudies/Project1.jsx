a109c35599025539edb7d158cceb0e97a6b0bf6a
AtienoObwanda <atienoobwanda@gmail.com>
2023-10-26 10:38:46 +0300

    Adjusting project section to use dynamic data

---
+++ src/CaseStudies/Project1.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Project1 = () => {
+    const Projects = [
+        {
+          name:'',
+          description:'',
+          duration:'',
+          link:'',
+          gitHub:'',
+          caseStudy:'',
+          poster:'',
+          gallery1:'',
+          gallery2:'',
+          gellery3:'',
+          gallery4: '',
+          video:'',
+        },
+        ]
+        
+  return (
+    <div>Project1</div>
+  )
+}
+
+export default Project1
