cbf5bd6de8f021f55863b72afe8b4a5dee159b05
AtienoObwanda <atienoobwanda@gmail.com>
2023-11-13 10:57:55 +0300

    updating contact details

--- src/Layout/Nav.jsx
+++ src/Layout/Nav.jsx
@@ -1,7 +1,8 @@
 import React,{useEffect, useState} from 'react'
 import {FaGithub,FaLinkedin,FaTwitter,FaWhatsapp,FaInstagram,FaPhone,FaMoon} from 'react-icons/fa'
-// BiLogoDevTo
+// 
 import {BsSun} from 'react-icons/bs'
+import {BiLogoDevTo} from 'react-icons/bi'
 
 
 
@@ -28,32 +29,32 @@ const Nav = () => {
   return (
       <nav className="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg dark:border-slate-600/60 
       dark:bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border">
-        <a href="#" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
+        <a href="https://github.com/AtienoObwanda/" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
         <FaGithub className='dark:orange-icon'/>
           <small className="text-center text-xs font-medium">  </small>
         </a>
 
-        <a href="#" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
+        <a href="https://www.linkedin.com/in/millicent-atieno/" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
         <FaLinkedin/>
           <small className="text-center text-xs font-medium">  </small>
         </a>
 
-        <a href="#" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
+        <a href="https://twitter.com/atien_o" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
           <FaTwitter/>
           <small className="text-center text-xs font-medium">  </small>
         </a>
 
-        <a href="#" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
-          <FaInstagram/>
+        <a href="https://dev.to/atienoobwanda" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
+          <BiLogoDevTo/>
           <small className="text-xs font-medium"></small>
         </a>
 
-        <a href="#" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
+        <a href="https://wa.me/+254700851861/?text=Hi%20Atieno" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
           <FaWhatsapp/>
           <small className="text-xs font-medium"></small>
         </a>
 
-        <a href="#" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
+        <a href="tel:+254700851861" className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800">
           <FaPhone/>
           <small className="text-xs font-medium"></small>
         </a>
