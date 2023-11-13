ac9a8e5c2f2723a5b13d221250a76b581b31768c
AtienoObwanda <atienoobwanda@gmail.com>
2023-11-13 11:11:34 +0300

    update github section

--- src/Components/GitHub.jsx
+++ src/Components/GitHub.jsx
@@ -4,13 +4,13 @@ import GitHubCalendar from "react-github-calendar";
 
 const GitHub = () => {
   return (
-    <div className="mt-[6em] mb-[6em] ">
+    <div className=" mt-[6em] mb-[6em] sm:hidden">
     <div className="ml-[-18em]  sm:ml-[-0.1em]">
     <h1 className='text-6xl  text-center text-dark dark:text-white font-newyork leading-normal  mb-8 sm:text-4xl'>GitHub Activity</h1>
 
     </div>
 {/* Skill logos slider */}
-<div className="hidden md relative m-auto overflow-hidden mr-[10em] ml-[10em] text-dark dark:text-white sm:w-full">
+<div className="relative m-auto overflow-hidden mr-[10em] ml-[10em] text-dark dark:text-white sm:w-full">
       {/* GiHub */}
       <GitHubCalendar
         username="AtienoObwanda"
